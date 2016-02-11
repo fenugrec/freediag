d5f3d80a825874cbc892610ed5c526027691d8bf
fenugrec <fenugrec@users.sourceforge.net>
2016-02-11 16:30:45 -0500

    diag_tty_win.c::diag_tty_break() : Removed auto adjustment of duration: Measurements taken with a logic analyzer confirm that on some systems (at least win7 + CH340 USB-serial), the call to SetCommBreak is extremely long (~6ms) which of course breaks iso14230 fast init. Instead of maintaining a self-adjusted correction value (which requires many iterations before settling to an adequate value, by design), diag_tty_break() instead considers the call to SetCommBreak() to be part of the break, and thence adjusts the subsequent call to diag_os_millisleep(). The call to ClearCommBreak() also takes a lot of time, but this cannot be corrected at the diag_tty level. Another commit will fix diag_l0_dumb's fastinit code to adjust to diag_tty_break() calls taking much longer than the requested breaktime.

--- scantool/diag_tty_win.c
+++ scantool/diag_tty_win.c
@@ -411,8 +411,7 @@ int diag_tty_iflush(struct diag_l0_device *dl0d)
 // ret 0 if ok
 int diag_tty_break(struct diag_l0_device *dl0d, const unsigned int ms) {
 	LARGE_INTEGER qpc1, qpc2;	//for timing verification
-	static long correction=0;	//running average offset (us) to add to the timeout
-	long real_t;	//"real" duration measured in us
+	long real_t;	//"real" duration
 	struct tty_int *wti = (struct tty_int *)dl0d->tty_int;
 	int errval=0;
 
@@ -421,36 +420,26 @@ int diag_tty_break(struct diag_l0_device *dl0d, const unsigned int ms) {
 		return diag_iseterr(DIAG_ERR_GENERAL);
 	}
 
-	if ( (ms + correction/1000)<1)
+	if (ms <= 1)
 		return diag_iseterr(DIAG_ERR_GENERAL);
 
 	QueryPerformanceCounter(&qpc1);
-	errval=!SetCommBreak(wti->fd);
-	diag_os_millisleep(ms + correction/1000);
+	errval = !SetCommBreak(wti->fd);
 	QueryPerformanceCounter(&qpc2);
-	errval += !ClearCommBreak(wti->fd);
+	//that call can take quite a while (6ms !!) on some setups (win7 + CH340 USB-Serial).
+	//It's still impossible to know (from here) when exactly TXD goes low (beginning or end of the call)
+	real_t=(long) (pf_conv * (qpc2.QuadPart-qpc1.QuadPart)) / 1000L;
+	real_t = (long) ms - real_t;	//time remaining
+	if (real_t <= 0) real_t = 0;
+	diag_os_millisleep((unsigned int ) real_t);
+
+	errval |= !ClearCommBreak(wti->fd);
 
 	if (errval) {
 		//if either of the calls failed
 		fprintf(stderr, FLFMT "tty_break could not set/clear break!\n", FL);
 		return diag_iseterr(DIAG_ERR_GENERAL);
 	}
-	real_t=(long) (pf_conv * (qpc2.QuadPart-qpc1.QuadPart));
-
-	//now verify if it's within 1ms of the requested delay.
-	real_t = real_t - (ms*1000);
-	if (real_t < -3000) {
-		diag_os_millisleep((unsigned int)(real_t / -1000));
-	} else if ((real_t > -1000) && (real_t < 1000)) {
-		//good enough:
-		return 0;
-	}
-	//we're here if we were off by more than -3ms or +1ms.
-	//correct by a fraction of the error.
-	//diag_os_millisleep also does some self-correcting; we don't want to overdo it.
-	correction = correction - (real_t / 3);
-	//fprintf(stderr, FLFMT "tty_break off by %ldus, new correction=%ldus.\n",
-	//	FL, real_t, correction);
 
 	return 0;
 }
