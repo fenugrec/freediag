ebef210654dec03ee675c8550b2b18b1f7ffeb00
Adam Goldman <adamg@pobox.com>
2016-11-13 15:18:32 -0800

    Add KWP6227 L2 protocol

--- scantool/diag_l2.h
+++ scantool/diag_l2.h
@@ -180,7 +180,8 @@ struct diag_l2_conn
 #define DIAG_L2_PROT_VAG	6	/* VAG ISO9141 based protocol */
 #define DIAG_L2_PROT_MB1	7	/* MB protocol 1 */
 #define DIAG_L2_PROT_MB2	8	/* MB protocol 2 */
-#define DIAG_L2_PROT_MAX	9	/* Maximum number of protocols */
+#define DIAG_L2_PROT_KWP6227	9	/* KWP6227 (keyword D3 B0 for Volvo) */
+#define DIAG_L2_PROT_MAX	10	/* Maximum number of protocols */
 
 /*
  * l2proto_list : static-allocated list of supported L2 protocols.
