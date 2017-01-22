388ede00d8f8f7a0be4c8f8ee2c6393eeb0786e6
Adam Goldman <adamg@pobox.com>
2017-01-21 18:47:56 -0800

    Only send ATIIA command if target address isn't ELM default 0x33

--- scantool/diag_l0_elm.c
+++ scantool/diag_l0_elm.c
@@ -756,14 +756,11 @@ elm_initbus(struct diag_l0_device *dl0d, struct diag_l1_initbus_args *in)
 			if ((dev->elmflags & ELM_323_BASIC) && (in->addr != 0x33)) {
 				fprintf(stderr, FLFMT "elm_initbus: ELM323 doesn't support target address %02X", FL, in->addr);
 				return diag_iseterr(DIAG_ERR_GENERAL);
-			} else {
+			} else if (in->addr != 0x33) {
 				uint8_t iia[15];
 				sprintf((char *) iia, "ATIIA %02X\x0D", in->addr);
 				rv=elm_sendcmd(dl0d, iia, 9, 500, NULL);
-				if (rv < 0 && in->addr == 0x33) {
-					fprintf(stderr, FLFMT "elm_initbus: ATIIA 33 failed, continuing anyway\n", FL);
-					rv = 0;
-				} else if (rv < 0) {
+				if (rv < 0) {
 					fprintf(stderr, FLFMT "elm_initbus: ATIIA %02X failed\n", FL, in->addr);
 					return diag_iseterr(DIAG_ERR_GENERAL);
 				}
