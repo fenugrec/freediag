8fc9016dc11193b95b8da0a00e9405c8723ed153
Adam Goldman <adamg@pobox.com>
2017-02-04 00:58:48 -0800

    850: tweak ecu descriptions

--- scantool/scantool_850.c
+++ scantool/scantool_850.c
@@ -60,20 +60,20 @@ static struct ecu_info ecu_list[] = {
 	 * Address 0x10 communicates by KWP71 protocol, so we currently
 	 * can't talk to it.
 	 */
-	{0x10, "m43", "Motronic M4.3 (DLC pin 3)", "EFI"}, /* 12700bps */
-	{0x10, "m44old", "Motronic M4.4 (old protocol)", "EFI"}, /* 9600bps */
+	{0x10, "m43", "Motronic M4.3 engine management (DLC pin 3)", "EFI"}, /* 12700bps */
+	{0x10, "m44old", "Motronic M4.4 engine management (old protocol)", "EFI"}, /* 9600bps */
 #endif
 	{0x11, "msa", "MSA 15.7 engine management (diesel vehicles)" ,"EFI"},
 	/* 0x13 - Volvo Scan Tool */
 	{0x18, "add", "912-D fuel-driven heater (cold climate option)", "HEA"},
 	{0x29, "ecc", "electronic climate control", "ECC"},
-	{0x2d, "vgla", "Volvo Guard Lock and Alarm", "GLA"},
+	{0x2d, "vgla", "alarm", "GLA"},
 	{0x2e, "psl", "left power seat", "PSL"},
 	{0x2f, "psr", "right power seat", "PSR"},
 	/* 0x33 - J1979 OBD2 */
 	{0x41, "immo", "immobilizer", "IMM"},
 	{0x51, "combi", "combined instrument panel", "CI"},
-	{0x58, "srs", "airbags and belt tensioner", "SRS"},
+	{0x58, "srs", "airbags", "SRS"},
 	{0x6e, "aw50", "AW50-42 transmission", "AT"},
 	{0x7a, "m44", "Motronic M4.4 engine management", "EFI"},
 	{0, NULL, NULL, NULL}
