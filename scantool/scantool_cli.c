fenugrec/freediag

commit 2f584eb715bf676ad01cc913b7dc833c08de6eda

cli: stop processing file if cmd not found
a typo or error in an .ini file can go unnoticed since cmd_source
continues to process other commands.
Adjusted testsuite accordingly

scantool/scantool_cli.c
@@ -318,6 +318,11 @@ static void readline_init(UNUSED(const struct cmd_tbl_entry *cmd_table)) {}
 
 #endif	//HAVE_LIBREADLINE
 
+/** get input from user or file
+*
+* @instream either stdin or file
+* @return NULL if no more input
+*/
 static char *
 command_line_input(const char *prompt, FILE *instream) {
 	if (instream == stdin) {
@@ -570,9 +575,15 @@ static const struct cmd_tbl_entry *find_cmd(const struct cmd_tbl_entry *cmdt, co
 
 static char nullstr[2] = {0,0};	//can't be const char because it goes into argv
 
-/*
- * CLI, returns results as CMD_xxx (such as CMD_EXIT)
- * If argc is supplied, then this is one shot cli, ie run the command
+/** CLI processor
+ *
+ * @prompt will be prompted only if argc==0
+ * @argc  If supplied, then this is one shot cli, ie run the command
+ * @instream stdin or file to source commands
+ *
+ * @return results as CMD_xxx (such as CMD_EXIT)
+ *
+ * prints *prompt,
  */
 static int
 do_cli(const struct cmd_tbl_entry *cmd_tbl, const char *prompt, FILE *instream, int argc, char **argv) {
@@ -592,7 +603,8 @@ do_cli(const struct cmd_tbl_entry *cmd_tbl, const char *prompt, FILE *instream, 
 	current_cmd_level = cmd_tbl;
 #endif
 
-	rv = CMD_FAILED, done = 0;
+	rv = CMD_FAILED;
+	done = 0;
 	snprintf(promptbuf, PROMPTBUFSIZE, "%s> ", prompt);
 	while (!done) {
 		char *inptr, *s;
@@ -604,9 +616,6 @@ do_cli(const struct cmd_tbl_entry *cmd_tbl, const char *prompt, FILE *instream, 
 			}
 			input = command_line_input(promptbuf, instream);
 			if (!input) {
-				if (instream == stdin) {
-					printf("\n");
-				}
 				break;
 			}
 
@@ -645,8 +654,8 @@ do_cli(const struct cmd_tbl_entry *cmd_tbl, const char *prompt, FILE *instream, 
 
 		if (ctp == NULL) {
 			printf("Unrecognized command. Try \"help\"\n");
-			if (argc) {
-				//was a single command : exit this level of do_cli()
+			if (instream != stdin) {
+				//processing a file : abort
 				break;
 			}
 			//else : continue getting input

scantool/scantool_cli.h
@@ -66,7 +66,7 @@ void enter_cli(const char *name, const char *initscript, const struct cmd_tbl_en
 
 
 /** Prompt for some input.
- * Returns a new 0-terminated string with trailing CR/LF stripped
+ * @return a new 0-terminated string with trailing CR/LF stripped, NULL if no more input
  *
  * Caller must free returned buffer. Used if we don't
  * have readline, and when reading init or command files.

tests/cli_1.ini
@@ -7,14 +7,19 @@ show
 #usage in subcmd, must not leave level
 probe ?
 ?
+up
+#usage through subcmd
+diag probe ?
+
+diag
 #cmd error in subcmd, must not leave level
 sr 2
 up
-#unavailabe cmd through subcmd table:
-diag badcmd
-#unavailable cmd @ root :
-probe
 
-#usage through subcmd
-diag probe ?
+
+#unavailable cmd @ root. stops processing this file, which means we can't quit anymore.
+#probe
+#unavailabe cmd through subcmd table, same problem
+#diag badcmd
+
 quit

tests/cli_1.stdo_p
@@ -1,1 +1,1 @@
-testerid.*age: probe.*vailable com.*Not conn.*Try \"h.*Try \"h.*age: probe
+testerid.*age: probe.*vailable com.*Not conn

commit c88053add04926ba3c29609c87f51d79283fa4ce

CLI : fix endless loop with invalid subcommand
When giving e.g. "diag lkfnds", the inner do_cli() runs in an endless
loop. Bug was introduced by 2f584eb715bf676ad01cc913b7dc833c08de6eda

scantool/scantool_cli.c
@@ -665,8 +665,8 @@ do_cli(const struct cmd_tbl_entry *cmd_tbl, const char *prompt, FILE *instream, 
 
 		if (ctp == NULL) {
 			printf("Unrecognized command. Try \"help\"\n");
-			if (instream != stdin) {
-				//processing a file : abort
+			if ((instream != stdin) || (argc > 0)) {
+				//processing a file, or running a subcommand : abort
 				break;
 			}
 			//else : continue getting input